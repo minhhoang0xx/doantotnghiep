9a5a9e36922f9235414f28c572ca7d1ac45edfc8
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2024-10-05 11:41:12 +0700

    Token

---
+++ server/src/services/JwtService.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const generalAccessToken = (payload) =>{
+    const access_token = jwt.sign(
+        {...payload},process.env.KEY, {expiresIn: '1h'}
+    )// ...payload trải các thuộc tính của đối tượng ra và có thể bổ sung hoặc thay đổi chúng trước khi truyền vào hàm.
+    // payload truyền toàn bộ đối tượng vào hàm mà không thay đổi cấu trúc của nó.
+    return access_token
+} 
+
+const generalRefreshToken = (payload) => {
+    const refresh_token = jwt.sign(
+        {...payload}
+    ,process.env.KEY, { expiresIn: '90d' });
+    
+    return refresh_token;
+}
+
+const refreshTokenJwt = (token) => {// cap token moi khi refreshtoken cu het han
+    return new Promise((resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.KEY, async (err, user) =>{
+                if(err){
+                    resolve({
+                        status :"rTokenJwt error",
+                        message: err
+                    })
+                }
+                const payload = user
+                const access_token = await generalAccessToken({
+                    id: payload?.id,
+                    isAdmin :payload?.isAdmin
+                })
+                console.log('123',access_token)
+                resolve ({
+                    status: 'OK',
+                    message: 'success',
+                    access_token
+
+                })
+            })
+        }catch(err){
+            reject(err);
+        }
+    })
+}
+
+
+
+module.exports = {
+    generalAccessToken,
+    generalRefreshToken,
+    refreshTokenJwt
+}
