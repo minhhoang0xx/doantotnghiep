c334b727c053af4e22a670d0e7e7260e77f8cac4
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2024-10-07 17:14:14 +0700

    Header

--- client/src/components/HeaderComponent/HeaderComponent.jsx
+++ client/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Col, Input } from 'antd'
+import { WrapperAccount, WrapperHeader, WrapperText } from './style';
+import { UserOutlined, CaretDownOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 
-const HeaderComponent = () =>{
+const HeaderComponent = () => {
     return (
-        <div>Header</div>
+        <div>
+            <WrapperHeader>
+                <Col span={6}>
+                    <WrapperText>Hoang System Education</WrapperText>
+                </Col>
+                <Col span={14}>
+                    <Input.Search placeholder="input search text" allowClear style={{ width: '90%' }} />
+                </Col>
+                <Col span={4}>
+                    <WrapperAccount>
+                            <UserOutlined style={{ fontSize:'30px'}} />
+                            <div>
+                               <span>Account</span> 
+                               <CaretDownOutlined/>
+                            </div>
+                            <ShoppingCartOutlined style={{ fontSize:'30px'}} />
+                            <span>Cart</span> 
+                    </WrapperAccount>
+                </Col>
+
+            </WrapperHeader>
+        </div>
     )
 }
 export default HeaderComponent
---
+++ client/src/components/HeaderComponent/style.js
@@ -0,0 +1,21 @@
+import{Row} from 'antd';
+import styled from 'styled-components'
+
+export const WrapperHeader = styled(Row)`
+    padding: 10px 120px;
+    background-color: darkgreen;
+`
+
+export const WrapperText = styled.span`
+    font-size: 18px;
+    font-weight: bold;
+    color: white;
+    text-align: left;
+`
+export const WrapperAccount = styled.span`
+    color: white;
+    display: flex;
+    text-align: left;
+    gap: 10px;
+    font-size: 15px
+`
