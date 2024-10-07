854de018bcb2521e594016785d3c6b5ce38afb6c
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2024-10-07 16:05:38 +0700

    Route FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,31 @@
 // import logo from './logo.svg';
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slices/counterSlice'
-import styled from 'styled-components'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {routes} from './routes';
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import { Fragment } from 'react';
+
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-const Button = styled.button({
-  background: 'red',
-});
-
 
   return (
     <div>
-      <div>
-        <Button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return(
+              <Route key = {route.path} path={route.path} element={
+                <Layout>
+                <Page/>
+                </Layout>
+              }/>
+            )
+          })}
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
---
+++ client/src/pages/NotFound/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NotFoundPage = () =>{
+    return (
+        <div> NOT FOUND</div>
+        )
+}
+ export default NotFoundPage
